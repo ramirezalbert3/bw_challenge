87f7bcd710dd60a3e206aa6fc9039b5597e92eb0
Albert <ramirezalbert3@gmail.com>
2018-10-12 01:25:32 +0200

    initial flask implementation for ex2

---
+++ ex2/server.py
@@ -0,0 +1,28 @@
+import re
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/password_check', methods=['GET'])
+def password_check():
+    # semi-customizable rules here
+    rules_dict =  {'length': 10,
+                   'must_have_numbers': True,
+                   'must_have_caps': True}
+
+    password = request.get_json()['password']
+    str_len = len(password) >= rules_dict['length']
+    
+    has_numbers = True
+    if rules_dict['must_have_numbers']:
+        has_numbers = bool(re.search(r'\d', password))
+    
+    has_caps = True
+    if rules_dict['must_have_caps']:
+        has_caps = bool(re.search(r'[A-Z]+', password))
+    
+    return str(str_len and has_numbers and has_caps)
+
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(port=5002)
